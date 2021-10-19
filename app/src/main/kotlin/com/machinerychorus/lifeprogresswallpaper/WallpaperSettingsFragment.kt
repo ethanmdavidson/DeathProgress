8b69ad03a86904d163347ea108375856ddc1adf6
Ethan Davidson <ethanmdavidson@gmail.com>
2021-10-18 20:15:31 -0400

    fix hint not being displayed

--- app/src/main/kotlin/com/machinerychorus/lifeprogresswallpaper/WallpaperSettingsFragment.kt
+++ app/src/main/kotlin/com/machinerychorus/lifeprogresswallpaper/WallpaperSettingsFragment.kt
@@ -1,6 +1,7 @@
 package com.machinerychorus.lifeprogresswallpaper
 
 import android.os.Bundle
+import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.machinerychorus.lifeprogresswallpaper.customPrefs.DateDialogPreference
@@ -9,6 +10,11 @@ class WallpaperSettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        //androidx EditTextPreference doesn't respect the hint attribute in the xml for some reason
+        //we set it here to work around that
+        val textPref = findPreference<EditTextPreference>(getString(R.string.goalsKey))
+        textPref?.setOnBindEditTextListener { editText -> editText.setHint(R.string.goalsHint) }
     }
 
     override fun onDisplayPreferenceDialog(preference: Preference?) {
