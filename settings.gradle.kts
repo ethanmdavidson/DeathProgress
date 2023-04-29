dc47c55d4a27e93fd7ce1c3fd069985381e64f0b
Ethan Davidson <ethanmdavidson@gmail.com>
2023-04-29 15:54:29 -0400

    go back to xml prefs
    because the colorPickerPreference isn't working right when created in code (weird things happening with alpha)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        mavenLocal()
         google()
         mavenCentral()
     }
