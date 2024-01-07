9defd842c63c432e8724c8b6842486e2e72ce6fc
Chris Howey <chris@howey.me>
2024-01-07 07:27:52 -0600

    add refresh to colorpicker

--- app/src/main/kotlin/com/machinerychorus/lifeprogresswallpaper/WallpaperSettingsFragment.kt
+++ app/src/main/kotlin/com/machinerychorus/lifeprogresswallpaper/WallpaperSettingsFragment.kt
@@ -2,11 +2,12 @@ package com.machinerychorus.lifeprogresswallpaper
 
 import android.graphics.Color
 import android.os.Bundle
+import androidx.core.content.edit
 import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.machinerychorus.lifeprogresswallpaper.customPrefs.DateDialogPreference
-import com.skydoves.colorpickerpreference.ColorPickerPreference
+import com.machinerychorus.lifeprogresswallpaper.customPrefs.ColorPreference
 import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
 import com.skydoves.colorpickerview.preference.ColorPickerPreferenceManager
 
@@ -24,7 +25,7 @@ class WallpaperSettingsFragment : PreferenceFragmentCompat() {
 		val manager = ColorPickerPreferenceManager.getInstance(context)
 
 		// listeners to keep hex and color selector in sync
-		val bgPref = findPreference<ColorPickerPreference>(getString(R.string.bgColorKey))
+		val bgPref = findPreference<ColorPreference>(getString(R.string.bgColorKey))
 		val bgHexPref = findPreference<EditTextPreference>(getString(R.string.bgColorHexKey))
 		bgPref?.getColorPickerView()?.setColorListener(ColorEnvelopeListener { envelope, _ ->
 			bgHexPref?.text = "#"+envelope.hexCode
@@ -36,12 +37,16 @@ class WallpaperSettingsFragment : PreferenceFragmentCompat() {
 			manager.clearSavedBrightnessSlider(getString(R.string.bgColorKey))
 			manager.setColor(getString(R.string.bgColorKey), Color.parseColor(newValue.toString()))
 			manager.restoreColorPickerData(bgPref?.getColorPickerView())
-			// TODO: need way to redraw/refresh color_box
+			preferenceManager
+				.sharedPreferences?.edit {
+					putInt(getString(R.string.bgColorKey), Color.parseColor(newValue.toString()))
+				}
+			bgPref?.refresh()
 			true
 		}
 
 		// listeners to keep hex and color selector in sync
-		val fgPref = findPreference<ColorPickerPreference>(getString(R.string.fgColorKey))
+		val fgPref = findPreference<ColorPreference>(getString(R.string.fgColorKey))
 		val fgHexPref = findPreference<EditTextPreference>(getString(R.string.fgColorHexKey))
 		fgPref?.getColorPickerView()?.setColorListener(ColorEnvelopeListener { envelope, _ ->
 			fgHexPref?.text = "#"+envelope.hexCode
@@ -53,7 +58,11 @@ class WallpaperSettingsFragment : PreferenceFragmentCompat() {
 			manager.clearSavedBrightnessSlider(getString(R.string.fgColorKey))
 			manager.setColor(getString(R.string.fgColorKey), Color.parseColor(newValue.toString()))
 			manager.restoreColorPickerData(fgPref?.getColorPickerView())
-			// TODO: need way to redraw/refresh color_box
+			preferenceManager
+				.sharedPreferences?.edit {
+					putInt(getString(R.string.fgColorKey), Color.parseColor(newValue.toString()))
+				}
+			fgPref?.refresh()
 			true
 		}
 	}
---
+++ app/src/main/kotlin/com/machinerychorus/lifeprogresswallpaper/customPrefs/ColorPreference.kt
@@ -0,0 +1,195 @@
+/*
+ * Copyright (C) 2018 skydoves
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("unused", "MemberVisibilityCanBePrivate")
+
+package com.machinerychorus.lifeprogresswallpaper.customPrefs
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.edit
+import androidx.preference.Preference
+import androidx.preference.PreferenceViewHolder
+import com.machinerychorus.lifeprogresswallpaper.R
+import com.skydoves.colorpickerview.ColorEnvelope
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.ColorPickerView
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
+import com.skydoves.colorpickerview.listeners.ColorListener
+import com.skydoves.colorpickerview.listeners.ColorPickerViewListener
+
+/**
+ * ColorPreference is a preference for persisting a chosen color by users.
+ * We can show a [ColorPickerDialog] and customize the dialog and [ColorPickerView].
+ */
+class ColorPreference : Preference {
+
+	private lateinit var colorBox: View
+	private lateinit var preferenceDialog: AlertDialog
+	private lateinit var preferenceColorPickerView: ColorPickerView
+
+	var preferenceColorListener: ColorPickerViewListener? = null
+	var defaultColor: Int = Color.BLACK
+	var cornerRadius: Int = 0
+	var paletteDrawable: Drawable? = null
+	var selectorDrawable: Drawable? = null
+	var dialogTitle: String? = null
+	var positive: String? = null
+	var negative: String? = null
+	var attachAlphaSlideBar = true
+	var attachBrightnessSlideBar = true
+
+	constructor(context: Context) : super(context)
+
+	constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+		getAttrs(attrs)
+		onInit()
+	}
+
+	constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+		context,
+		attrs,
+		defStyleAttr
+	) {
+		getAttrs(attrs, defStyleAttr)
+		onInit()
+	}
+
+	private fun getAttrs(attrs: AttributeSet) {
+		val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ColorPickerPreference)
+		try {
+			setTypeArray(typedArray)
+		} finally {
+			typedArray.recycle()
+		}
+	}
+
+	private fun getAttrs(attrs: AttributeSet, defStyle: Int) {
+		val typedArray =
+			context.obtainStyledAttributes(attrs, R.styleable.ColorPickerPreference, defStyle, 0)
+		try {
+			setTypeArray(typedArray)
+		} finally {
+			typedArray.recycle()
+		}
+	}
+
+	private fun setTypeArray(typedArray: TypedArray) {
+		defaultColor =
+			typedArray.getColor(R.styleable.ColorPickerPreference_default_color, defaultColor)
+		cornerRadius =
+			typedArray.getDimensionPixelSize(R.styleable.ColorPickerPreference_preference_colorBox_radius, cornerRadius)
+		paletteDrawable = typedArray.getDrawable(R.styleable.ColorPickerPreference_preference_palette)
+		selectorDrawable = typedArray.getDrawable(R.styleable.ColorPickerPreference_preference_selector)
+		dialogTitle = typedArray.getString(R.styleable.ColorPickerPreference_preference_dialog_title)
+		positive = typedArray.getString(R.styleable.ColorPickerPreference_preference_dialog_positive)
+		negative = typedArray.getString(R.styleable.ColorPickerPreference_preference_dialog_negative)
+		attachAlphaSlideBar =
+			typedArray.getBoolean(
+				R.styleable.ColorPickerPreference_preference_attachAlphaSlideBar,
+				attachAlphaSlideBar
+			)
+		attachBrightnessSlideBar =
+			typedArray.getBoolean(
+				R.styleable.ColorPickerPreference_preference_attachBrightnessSlideBar,
+				attachBrightnessSlideBar
+			)
+	}
+
+	fun onInit() {
+		widgetLayoutResource = R.layout.layout_colorpicker_preference
+		preferenceDialog = ColorPickerDialog.Builder(context).apply {
+			setTitle(dialogTitle)
+			setPositiveButton(
+				positive,
+				ColorEnvelopeListener { envelope, _ ->
+					if (colorBox.background is GradientDrawable) {
+						(colorBox.background as GradientDrawable).setColor(envelope.color)
+						notifyColorChanged(envelope)
+						preferenceManager
+							.sharedPreferences?.edit {
+								putInt(key, envelope.color)
+							}
+					}
+				}
+			)
+			setNegativeButton(negative) { dialogInterface, _ -> dialogInterface.dismiss() }
+			attachAlphaSlideBar(attachAlphaSlideBar)
+			attachBrightnessSlideBar(attachBrightnessSlideBar)
+			this@ColorPreference.preferenceColorPickerView = this.colorPickerView.apply {
+				paletteDrawable?.let { setPaletteDrawable(it) }
+				selectorDrawable?.let { setSelectorDrawable(it) }
+				preferenceName = key
+				setInitialColor(defaultColor)
+			}
+		}.create()
+	}
+
+	private fun notifyColorChanged(envelope: ColorEnvelope) {
+		preferenceColorListener?.let {
+			if (it is ColorListener) {
+				it.onColorSelected(envelope.color, true)
+			} else if (it is ColorEnvelopeListener) {
+				it.onColorSelected(envelope, true)
+			}
+		}
+	}
+
+	override fun onBindViewHolder(holder: PreferenceViewHolder) {
+		super.onBindViewHolder(holder)
+		colorBox = holder.findViewById(R.id.preference_colorBox)
+		colorBox.background = GradientDrawable().apply {
+			cornerRadius = this@ColorPreference.cornerRadius.toFloat()
+			setColor(
+				if (key == null) {
+					this@ColorPreference.defaultColor
+				} else {
+					preferenceManager.sharedPreferences!!.getInt(key, this@ColorPreference.defaultColor)
+				}
+			)
+		}
+	}
+
+	fun refresh() {
+		colorBox.background = GradientDrawable().apply {
+			cornerRadius = this@ColorPreference.cornerRadius.toFloat()
+			setColor(
+				if (key == null) {
+					this@ColorPreference.defaultColor
+				} else {
+					preferenceManager.sharedPreferences!!.getInt(key, this@ColorPreference.defaultColor)
+				}
+			)
+		}
+	}
+
+	override fun onClick() {
+		super.onClick()
+		preferenceDialog.show()
+	}
+
+	/** gets an [AlertDialog] that created by preferences. */
+	fun getPreferenceDialog(): AlertDialog = preferenceDialog
+
+	/** gets a [ColorPickerView] that created by preferences. */
+	fun getColorPickerView(): ColorPickerView = preferenceColorPickerView
+}
